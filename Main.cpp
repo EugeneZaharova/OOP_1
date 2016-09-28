a2b5c7171c03ca6af4615d38f1cd7a5fa0719c58
EugeneZaharova <eugenezaharova@mail.ru>
2016-09-28 21:42:49 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -11,18 +11,15 @@ int main() {
 	trian->Print(); 
 	cout << trian->Square() << endl;
 	delete trian;
-	system("pause");
 	
 	Figure *foursq = new Foursquare(); 
 	foursq->Print(); 
 	cout << foursq->Square() << endl;
 	delete foursq;
-	system("pause");
 	
 	Figure *octag = new Octagon(); 
 	octag->Print(); 
 	cout << octag->Square() << endl;
 	delete octag;
-	system("pause");
 	return 0; 
 }
